e5c1a1eedbf8d8607492cdfebd2ec55043a733dd
Harsh <harshuniverse000@gmail.com>
2024-08-01 12:07:02 +0530

    component are statefull

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,29 @@
+"use client"
+import { useState } from "react";
+
 import SubtleCheckbox from "@/components/Checkbox/Subtle/SubtleCheckbox";
 import ToggleSubtle from "@/components/Toggle/Subtle/toggle";
 
 export default function Home() {
+  const [subtleToggle, setSubtleToggle] = useState(false);
+  const [subtleCheckbox, setSubtleCheckbox] = useState(false);
+
   return (
     <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+
       <ToggleSubtle
         size="46px"
+        value={subtleToggle}
+        onChange={setSubtleToggle}
       />
+
       <SubtleCheckbox
         size='1em'
         strokeColor='black'
         strokeWidth={6}
+
+        value={subtleCheckbox}
+        onChange={setSubtleCheckbox}
       />
     </div>
   )
